e3af7818811bc34059e695901e589d8abc1cf4cb
Victor Valencia <sosviperdigital@gmail.com>
2022-02-12 00:35:11 -0500

    Cambios para el despliegue a Heroku

---
+++ src/main/java/com/invoice2peru/market/PlatziMarketApplication.java
@@ -0,0 +1,13 @@
+package com.invoice2peru.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatziMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatziMarketApplication.class, args);
+	}
+
+}
